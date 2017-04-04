a0c8edd1056fada089755e46ffa09f8c64bc4267
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-04-04 09:52:33 +0200

    before demo

---
+++ mod04/demo.js
@@ -0,0 +1,25 @@
+class Calculator {
+  add(x, y) {
+    return x + y;
+  }
+
+  subtract(x, y) {
+    return x - y;
+  }
+}
+
+var calculator = new Calculator();
+
+var x = document.getElementById("x");
+var y = document.getElementById("y");
+var result = document.getElementById("result");
+
+document.getElementById("add").addEventListener("click", () => {
+  var sum = calculator.add(x.value, y.value);
+  result.innerText = sum;
+});
+
+document.getElementById("subtract").addEventListener("click", () => {
+  var difference = calculator.subtract(x.value, y.value);
+  result.innerText = difference;
+});
