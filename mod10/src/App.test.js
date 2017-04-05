18b91b4c26071aad7e52edc8ecc26c27f0b8b572
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-04-05 16:00:39 +0200

    Before eject

---
+++ mod10/src/App.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import App from "./App";
+import renderer from "react-test-renderer";
+import Counter from "./Counter";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
+
+it("renders an `.App-logo`", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("img.App-logo").length).toBe(1);
+});
+
+it("Can incement", () => {
+  const wrapper = shallow(<Counter />);
+  wrapper.find("button").simulate("click");
+  wrapper.find("button").simulate("click");
+
+  expect(wrapper.state("counter")).toBe(2);
+});
+
+it("Counter renders correctly", () => {
+  const tree = renderer.create(<Counter />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
