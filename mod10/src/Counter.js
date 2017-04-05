318afa7bcefd728ab5ba3678dde3c769540071b5
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-04-05 16:51:01 +0200

    React tooling

--- mod10/src/Counter.js
+++ mod10/src/Counter.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    constructor(){
-        super();
-        this.state = {counter:0}
-        this.onClick = this.onClick.bind(this);
-    }
-    
-    onClick(){
-        this.setState({counter : this.state.counter + 1})
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this.onClick}>Click me...</button>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = { counter: 0 };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <button className="btn btn-default" onclick={this.onClick}>
+          Click the button
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
