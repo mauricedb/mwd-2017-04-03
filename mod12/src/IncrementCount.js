fc32632da3bd472155b00b32325a63fe9b885c1f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-04-06 10:31:41 +0200

    Before lab

---
+++ mod12/src/IncrementCount.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class IncrementCount extends Component {
+    render() {
+        return (
+            <div>
+                Count = {0}
+                <button>Increment</button>
+            </div>
+        );
+    }
+}
+
+export default IncrementCount;
